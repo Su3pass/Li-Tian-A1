74b0dfbeedef4cd644a4d549af91b2425db63903
Li Tian <1518061368@qq.com>
2024-11-20 01:31:17 +0800

    Part 3 – Using classes and objects
    Three GeneralPractitioner objects (GP_1, GP_2, GP_3) and two Specialist objects (SP_1 and SP_2) were created, representing general practitioners and specialists respectively.

--- Li_Tian-A1/src/AssignmentOne.java
+++ Li_Tian-A1/src/AssignmentOne.java
@@ -2,6 +2,24 @@
 // 点击装订区域中的 <icon src="AllIcons.Actions.Execute"/> 图标。
 public class AssignmentOne {
     public static void main(String[] args) {
+        // Part 3 – Using classes and objects
+        GeneralPractitioner GP_1 = new GeneralPractitioner("1011", "Dr. John Wick", "Experienced surgeon", "Senior Doctor", "General Medicine", "General health", "8 AM - 12 PM", "NULL");
+        GeneralPractitioner GP_2 = new GeneralPractitioner("7202", "Dr. Alice Mobley", "Family health expert", "General Practitioner", "General Medicine", "General health", "12 AM - 6PM", "NULL");
+        GeneralPractitioner GP_3 = new GeneralPractitioner("4032", "Dr. Mark Curry", "Rural health specialist", "General Practitioner", "General Medicine", "General health", "6 PM - 10 PM", "NULL");
 
+        Specialist SP_1 = new Specialist("1014", "Dr. Kobe White", "Dermatology expert", "Dermatologist", "Dermatology", "Skin diseases", "8 AM - 9 PM", "Dermatological Surgery", 20);
+        Specialist SP_2 = new Specialist("9821", "Dr. Evan Green", "ENT specialist", "Otolaryngologist", "ENT (Ear, Nose, and Throat)", "Ear, Nose, and Throat treatment", "12 AM - 12 PM", "ENT Surgery", 15);
+
+        System.out.println("------------------------------------------");
+        GP_1.printDetails();
+        System.out.println("------------------------------------------");
+        GP_2.printDetails();
+        System.out.println("------------------------------------------");
+        GP_3.printDetails();
+        System.out.println("------------------------------------------");
+        SP_1.printDetails();
+        System.out.println("------------------------------------------");
+        SP_2.printDetails();
+        System.out.println("------------------------------------------");
     }
 }
